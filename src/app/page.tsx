2160eb3158d8b0e1ec011d0b203539537905effe
Ravil <ravil.nell@gmail.com>
2023-09-01 23:05:14 +0400

    layout updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   const [value, setValue] = useState('')
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+    <main className="flex min-h-screen flex-col items-center justify-between lg:p-24 p-12">
+      <div className="max-w-5xl w-full items-center font-mono text-sm flex justify-center">
         <CurrencyCard currency="USD" value={value} onChange={setValue} />
       </div>
     </main>
